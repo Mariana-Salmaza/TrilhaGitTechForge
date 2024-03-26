43c7ff7968987bd018aefe2749f07cac686420f1
Mariana Salmaza <mariana.salmaza@grupointegrado.br>
2024-03-26 01:59:47 -0300

    created exercise 1

---
+++ Exercicio1.js
@@ -0,0 +1,11 @@
+// 1.Declare duas variáveis, a e b, e atribua valores numéricos a elas. Crie uma terceira variável, soma, e atribua a soma de a e b a ela. Imprima o resultado no console.
+
+// Declaração e atribuição de valores às variáveis 'a' e 'b'
+let a = 10;
+let b = 20;
+
+// Criação de uma terceira variável 'soma' e atribuição da soma de 'a' e 'b' a ela
+let soma = a + b;
+
+// resultado no console
+console.log("A soma de", a, "e", b, "é igual a", soma);
